49f4d4e28858b8c507b810fece71efec987993a6
Juan Quedena <juanquedena@gmail.com>
2019-08-20 06:26:44 -0500

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Container, Row, Col} from 'react-bootstrap';
+
+const Footer = props => {
+    return (
+        <div className="footer ">
+            <Container>
+                <Row>d</Row>
+            </Container>
+        </div>
+    );
+};
+
+Footer.propTypes = {
+    
+};
+
+export default Footer;
